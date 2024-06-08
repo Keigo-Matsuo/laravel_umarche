77546d5c0c63ec6b1dfc4025b42cadb5f5a7fb3b
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-08 16:41:20 +0900

    props
    クラスコンポーネントと競合してpropsが動作しない

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     public function showComponent1() {
-        return view('tests.component-test1'); // フォルダ名.ファイル名
+        $message = 'メッセージ';
+        return view('tests.component-test1', compact('message')); // フォルダ名.ファイル名
     }
 
     public function showComponent2() {
