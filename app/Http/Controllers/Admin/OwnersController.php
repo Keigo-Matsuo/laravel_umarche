27a30733b7402625e0def07b8b2aa8df6a1d3a47
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-10 14:17:54 +0900

    オーナーindex, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -27,9 +27,9 @@ class OwnersController extends Controller
         // echo $date_now->year;
         // echo $date_parse;
 
-        $e_all = Owner::all();
+        // $e_all = Owner::all();
 
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
         
         // $c_test = collect([
@@ -38,7 +38,9 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
 
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -48,7 +50,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
