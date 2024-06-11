1001bbf818c45d7543ce003827e577962981fc22
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-11 22:35:40 +0900

    image index

--- resources/views/components/shop-thumbnail.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if(empty($filename))
-        <img src="{{ asset('images/no_image.jpg') }}" alt="No Image">
-    @else
-        <img src="{{ asset('storage/shops/' . $filename) }}" alt="Shop Image">
-    @endif
-</div>
---
+++ resources/views/components/thumbnail.blade.php
@@ -0,0 +1,19 @@
+@php
+
+if ($type === 'shops') {
+    $path = 'storage/shops/';
+}
+
+if ($type === 'products') {
+    $path = 'storage/products/';
+}
+
+@endphp
+
+<div>
+    @if(empty($filename))
+        <img src="{{ asset('images/no_image.jpg') }}" alt="No Image">
+    @else
+        <img src="{{ asset($path . $filename) }}" alt="Shop Image">
+    @endif
+</div>
