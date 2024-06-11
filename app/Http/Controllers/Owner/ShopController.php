a319605b226362c94662e334b66994d628d8c3bd
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-11 14:51:54 +0900

    index画面

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -30,8 +30,8 @@ class ShopController extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id(); // ログインしているユーザーのidを取得
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        // $ownerId = Auth::id(); // ログインしているユーザーのidを取得
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index', compact('shops'));
     }
