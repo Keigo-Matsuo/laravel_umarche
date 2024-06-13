0180c0f464c5663ee5548c82efdbab48ee616fe6
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-13 12:49:20 +0900

    route, itemController, index

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@sample.com',
+            'password' => Hash::make('password1234'),
+            'created_at' => '2024/06/10 11:50:00'
+        ]);
+    }
+}
