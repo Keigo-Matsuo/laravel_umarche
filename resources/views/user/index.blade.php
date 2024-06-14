708fbe0c528572c68e931d9551d48098abec9d86
Keigo Matsuo <keigo.matsuo@g.dreamcareer.co.jp>
2024-06-14 16:28:05 +0900

    表示件数

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -41,7 +41,31 @@
                                 </option>
                             </select>
                         </div>
-                        <div>表示件数</div>
+
+                        <div>
+                            <span class="text-sm">表示件数</span><br>
+                            <select name="pagination" id="pagination">
+
+                                <option value="20"
+                                    @if(\Request::get('pagination') === '20')
+                                    selected
+                                    @endif>20件
+                                </option>
+
+                                <option value="50"
+                                    @if(\Request::get('pagination') === '50')
+                                    selected
+                                    @endif>50件
+                                </option>
+
+                                <option value="100"
+                                    @if(\Request::get('pagination') === '100')
+                                    selected
+                                    @endif>100件
+                                </option>
+
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -71,6 +95,10 @@
                         @endforeach
                     </div>
 
+                    {{ $products->appends([
+                        'sort' => \Request::get('sort'),
+                        'pagination' => \Request::get('pagination')
+                    ])->links() }}
                 </div>
             </div>
         </div>
@@ -82,6 +110,10 @@
         this.form.submit();
     });
 
+    const paginate = document.getElementById('pagination');
+    paginate.addEventListener('change', function() {
+        this.form.submit();
+    });
 </script>
 
 </x-app-layout>
